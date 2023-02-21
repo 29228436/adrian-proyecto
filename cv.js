fe001549d077eb4ca63b30fa6e5b0fe6c56447d9
Adrian Mayer <adrianguillermo2017@gmail.com>
2023-02-20 23:57:56 -0300

    Primer escritura en el repositorio

---
+++ cv.js
@@ -0,0 +1,17 @@
+
+
+  function myFunction() {
+    console.log("Año");
+    document.getElementById("demo").innerHTML = "1988";
+  }
+ 
+  function myFunction1() {
+    console.log("Escuela");
+    document.getElementById("demo1").innerHTML = "Escuela N°4";
+  }
+  
+  function myFunction2() {
+    console.log("Habilidades");
+    document.getElementById("demo2").innerHTML = "3";
+  }
+  
